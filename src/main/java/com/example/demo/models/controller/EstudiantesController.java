2457cb912b09ebfc2da17215afc0efb0beb82e40
Juan Amezquita <Juan.david.amezquita2001@gmail.com>
2022-03-18 12:05:35 -0500

    Inicial commit

--- src/main/java/com/example/demo/models/controller/EstudiantesController.java
+++ src/main/java/com/example/demo/models/controller/EstudiantesController.java
@@ -1,15 +1,16 @@
 package com.example.demo.models.controller;
 
 import com.example.demo.models.services.IEstudianteService;
+import lombok.Generated;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 @Controller
 
 public class EstudiantesController {
 
     @Autowired
     private IEstudianteService EstudianteService;
-
 
 
 }
